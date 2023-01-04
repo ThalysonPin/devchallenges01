3534864b4c69b7f2b0a584ad886eee9e2f73c685
Thalyson Pin <thalyson_emanuels.p@hotmail.com>
2023-01-04 11:57:56 -0400

    Finished.

--- devchallenge/src/components/differentBtns/ButtonsComp.jsx
+++ devchallenge/src/components/differentBtns/ButtonsComp.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './buttons1.css'
 import Button from '@mui/material/Button';
 import { useState } from 'react';
-
+import { FormControl, InputLabel, MenuItem, Modal, Select, Switch } from '@mui/material';
+import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
 
 /*
 User story: I can see different button types: default, outline and text
@@ -16,12 +17,19 @@ User story: I can choose to disable box-shadow
 User story: I can choose to disable the button
 User story: I can choose to have an icon on the left or right (Use Google Icon and at least 5 variants)
 User story: I can still access all button attributes
-
 */
 
 export default function ButtonsComp() {
   const [classBtn, setClassBtn] = useState("class-btn-default")
-  const [status, setStatus] = useState()
+  const [styleBtn, setStyleBtn] = useState("contained")
+  const [sizeBtn, setSizeBtn] = useState("medium")
+  const [colorBtn, setColorBtn] = useState("primary")
+  const [disableBtn, setDisableBtn] = useState(false)
+  const [disabler, setDisabler] = useState("disable")
+  const [boxShadow, setBoxShadow] = useState(true)
+  const [leftIcon, setLeftIcon] = useState(false)
+  const [rightIcon, setRightIcon] = useState(false)
+
 
   return (
     <div className='containerButtons'>
@@ -52,10 +60,66 @@ export default function ButtonsComp() {
 
           <section>
             <div className='leftArea'>
-              <button class={classBtn} status> Edit </button>
+              <Button 
+              variant={styleBtn}
+              size={sizeBtn} 
+              color={colorBtn} 
+              disabled={disableBtn}
+              id={boxShadow ? "boxShadow" : "boxShadowNone"}
+              startIcon={leftIcon ? <ModeEditOutlineIcon/> : ""}
+              endIcon={rightIcon ? <ModeEditOutlineIcon/> : ""}
+
+              >Edit here</Button>
             </div>
 
             <div className='rightArea'>
+{/* ================================== style buttons =====================*/}
+              <select onChange={(e) => setStyleBtn(e.target.value)}>
+                  <option disabled selected hidden>Style Btn</option>
+                  <option value="text">Text</option>
+                  <option value="contained" >Default</option>
+                  <option value="outlined" >Outlined</option>
+              </select>
+{/* ================================== Size buttons =====================*/}
+              <select onChange={(e) => setSizeBtn(e.target.value)}>
+                <option disabled selected hidden>Size Btn</option>
+                <option value="small">Small</option>
+                <option value="medium">Medium</option>
+                <option value="large">Large</option>
+              </select>
+{/* ================================== Size buttons =====================*/}
+              <select onChange={(e) => setColorBtn(e.target.value)}>
+                <option disabled selected hidden>Color Btn</option>
+                <option value="primary">Primary</option>
+                <option value="secondary">Secondary</option>
+                <option value="error">Danger</option>
+                <option value="success">Success</option>
+              </select>
+
+              <h4>Disable button</h4>
+              <Switch 
+              checked={disableBtn}
+              onChange={(e) => setDisableBtn(e.target.checked)}
+              />
+              
+              <h4>Box Shadow</h4>
+              <Switch 
+              checked={boxShadow}
+              onChange={(e) => setBoxShadow(e.target.checked)}
+              />
+
+              <h4>Left Icon?</h4>
+              <Switch 
+              checked={leftIcon}
+              onChange={(e) => setLeftIcon(e.target.checked)}
+              />
+              
+              <h4>Right Icon?</h4>
+              <Switch 
+              checked={rightIcon}
+              onChange={(e) => setRightIcon(e.target.checked)}
+              />
+
 
             </div>
           </section>
